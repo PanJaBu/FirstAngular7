469c9709cd2a70a37eab889b51d7168c95fae8b6
PanJaBu <jakub@jagodno.pl>
2018-10-29 22:05:27 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'asseco-app';
+  title = 'My First Angular 7 Application';
 }
